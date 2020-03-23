1a33b6fc7e55b79f6af1312632d4fb3b66225bf2
togashi29 <saladwithsalad@yahoo.co.jp>
2020-03-23 22:12:42 +0900

    define generator

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,16 @@ module ManageOvertime
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
