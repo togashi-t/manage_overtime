6c7a31ed687cbc3df707df59ac38443d7652c964
togashi29 <saladwithsalad@yahoo.co.jp>
2020-03-25 22:44:58 +0900

    create overtimes table

--- db/migrate/20200325133938_create_overtimes.rb
+++ db/migrate/20200325133938_create_overtimes.rb
@@ -1,12 +1,14 @@
 class CreateOvertimes < ActiveRecord::Migration[5.2]
   def change
     create_table :overtimes do |t|
-      t.date :date
-      t.time :work_start_time
-      t.time :work_end_time
-      t.time :work_time
+      t.date :date, null: false
+      t.time :work_start_time, null: false
+      t.time :work_end_time, null: false
+      t.time :work_time, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :overtimes, [:user_id, :date], unique: true
   end
 end
