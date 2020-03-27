d02550bd19a08aa3ec225d95b42050e551f4037b
togashi29 <saladwithsalad@yahoo.co.jp>
2020-03-27 21:05:40 +0900

    overtime create action

--- app/controllers/overtimes_controller.rb
+++ app/controllers/overtimes_controller.rb
@@ -3,5 +3,13 @@ class OvertimesController < ApplicationController
   end
 
   def create
+    @overtime = current_user.overtimes.build(overtime_params)
+    @overtime.save!
   end
+
+
+  private
+    def overtime_params
+      params.require(:overtime).permit(:date, :work_start_time, :work_end_time, :work_time )
+    end
 end
