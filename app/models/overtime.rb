fa5bf9f48ee7d828b0b15311ae0fc778f6e2ac54
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-04 11:36:42 +0900

    define vartual attributes work_time

--- app/models/overtime.rb
+++ app/models/overtime.rb
@@ -1,3 +1,10 @@
 class Overtime < ApplicationRecord
   belongs_to :user
+  attr_accessor :work_time
+  before_validation :convert_work_time_to_work_time_minutes
+
+  private
+    def convert_work_time_to_work_time_minutes
+      self.work_time_minutes = (Tod::TimeOfDay.parse(self.work_time).to_i / 60)
+    end
 end
