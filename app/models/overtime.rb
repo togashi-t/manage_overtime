0a134bac1da4f9804ec15fab35f4b443385f0c66
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-05 07:46:40 +0900

    method of monthly_chart_data_of_a_user

--- app/models/overtime.rb
+++ app/models/overtime.rb
@@ -1,10 +1,28 @@
 class Overtime < ApplicationRecord
   belongs_to :user
   attr_accessor :work_time
-  before_validation :convert_work_time_to_work_time_minutes
+  before_validation :convert_work_time_to_work_time_minute
+
+  def self.monthly_chart_data(userid)
+    monthly_minute_data = Overtime.where(user_id: userid).group("DATE_FORMAT(date,'%Y年%m月')").sum(:work_time_minute)
+    monthly_hour_data = {}
+    monthly_minute_data.each do |key, value|
+      monthly_hour_data[key] = value / 60
+    end
+    monthly_hour_data
+  end
+
+  # def self.monthly_chart_data(user)
+  #   monthly_minute_data = user.overtimes.group("DATE_FORMAT(date,'%Y年%m月')").sum(:work_time_minute)
+  #   monthly_hour_data = {}
+  #   monthly_minute_data.each do |key, value|
+  #     monthly_hour_data[key] = value / 60
+  #   end
+  #   monthly_hour_data
+  # end
 
   private
-    def convert_work_time_to_work_time_minutes
-      self.work_time_minutes = (Tod::TimeOfDay.parse(self.work_time).to_i / 60)
+    def convert_work_time_to_work_time_minute
+      self.work_time_minute = Tod::TimeOfDay.parse(self.work_time).to_i / 60
     end
 end
