491850ce933184c463c2428fe67da56051b8b4a5
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-04 10:34:15 +0900

    migrate for add column

--- db/migrate/20200404013159_add_work_time_minutes_to_overtimes.rb
+++ db/migrate/20200404013159_add_work_time_minutes_to_overtimes.rb
@@ -1,5 +1,9 @@
 class AddWorkTimeMinutesToOvertimes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :overtimes, :work_time_minutes, :integer
+  def up
+    add_column :overtimes, :work_time_minutes, :integer, null: false
+  end
+
+  def down
+    remove_column :overtimes, :work_time_minutes, :integer, null: false
   end
 end
