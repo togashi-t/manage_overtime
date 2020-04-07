49d5ba5da2cacf7511866b72d03e15fd469e8184
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-07 20:47:54 +0900

    reject date_format()

--- app/models/overtime.rb
+++ app/models/overtime.rb
@@ -4,22 +4,14 @@ class Overtime < ApplicationRecord
   before_validation :convert_work_time_to_work_time_minute
 
   def self.monthly_chart_data(userid)
-    monthly_minute_data = Overtime.where(user_id: userid).group("DATE_FORMAT(date,'%Y年%m月')").sum(:work_time_minute)
+    # {[2019, 4]=>○○○○, [2019, 5]=>○○○○, …} を生成
+    year_and_month_minute_data = Overtime.where(user_id: userid).group("extract(year from date)").group("extract(month from date)").sum(:work_time_minute)
     monthly_hour_data = {}
-    monthly_minute_data.each do |key, value|
-      monthly_hour_data[key] = value / 60
+    year_and_month_minute_data.each do |key, value|
+      monthly_hour_data["#{key[0]}年#{key[1]}月"] = value / 60
     end
     monthly_hour_data
   end
-
-  # def self.monthly_chart_data(user)
-  #   monthly_minute_data = user.overtimes.group("DATE_FORMAT(date,'%Y年%m月')").sum(:work_time_minute)
-  #   monthly_hour_data = {}
-  #   monthly_minute_data.each do |key, value|
-  #     monthly_hour_data[key] = value / 60
-  #   end
-  #   monthly_hour_data
-  # end
 
   private
 
