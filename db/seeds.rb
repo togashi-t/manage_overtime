c17f525436b671ec9d2f1cedf701e361a3318727
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-09 22:48:39 +0900

    multiple user's seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-# テストユーザー情報
-NAME = "test_user".freeze
-GROUP = "A".freeze
-EMAIL = "test@example.com".freeze
-PASSWORD = "password".freeze
+# user
+GROUPS = ["A", "B", "C"]
+PASSWORD = "password"
+
+users = []
+3.times{
+  GROUPS.each do |group|
+    users << {
+      name: Gimei.unique.name.kanji,
+      group: group,
+      email: Faker::Internet.email,
+      password: PASSWORD
+    }
+  end
+}
+User.create!(users)
+puts "テストユーザーの初期データを投入しました"
+
+
 
 # overtime
 # データの入力期間
@@ -17,30 +31,25 @@ MIN_WORK_END_TIME_MINUTE = MIN_WORK_END_TIME.second_of_day / 60
 MAX_WORK_END_TIME = Tod::TimeOfDay.parse("24:00")
 MAX_WORK_END_TIME_MINUTE = MAX_WORK_END_TIME.second_of_day / 60
 
-# 入力確率 1/(RECORD_CONSTANT) の確率でデータを記録
-RECORD_CONSTANT = 3
-
-user = User.find_or_create_by!(email: EMAIL) do |user|
-  user.name = NAME
-  user.group = GROUP
-  user.password = PASSWORD
-  puts "テストユーザーの初期データインポートに成功しました。"
-end
-
-user.overtimes.destroy_all
+# 入力確率 1/rand(RECORD_CONSTANT_RANGE) の確率でデータを記録
+RECORD_CONSTANT_RANGE = 3..6
 
 overtimes = []
-(START_DATE..END_DATE).each do |date|
-  next unless rand(RECORD_CONSTANT).zero?
-
-  WORK_END_TIME = Tod::TimeOfDay.new(0) + rand(MIN_WORK_END_TIME_MINUTE..MAX_WORK_END_TIME_MINUTE) * 60
-  overtimes << {
-    user_id: user.id,
-    date: date,
-    work_start_time: WORK_START_TIME.to_s,
-    work_end_time: WORK_END_TIME.to_s,
-    work_time: (WORK_END_TIME - WORK_START_TIME).to_s,
-  }
+users = User.order("id")
+users.each do |user|
+  record_constant = rand(RECORD_CONSTANT_RANGE)
+  (START_DATE..END_DATE).each do |date|
+    if rand(record_constant).zero?
+      work_end_time = Tod::TimeOfDay.new(0) + rand(MIN_WORK_END_TIME_MINUTE..MAX_WORK_END_TIME_MINUTE) * 60
+      overtimes << {
+        user_id: user.id,
+        date: date,
+        work_start_time: WORK_START_TIME.to_s,
+        work_end_time: work_end_time.to_s,
+        work_time: (work_end_time - WORK_START_TIME).to_s,
+      }
+    end
+  end
 end
 Overtime.create!(overtimes)
-puts "残業日時の初期データ投入に成功しました。"
+puts "残業日時の初期データを投入しました。"
