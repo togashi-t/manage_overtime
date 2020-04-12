b30cc5365e983c591ed3af51e39b41ac1a3f1e6a
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-12 21:30:04 +0900

    user show table

--- app/helpers/overtimes_helper.rb
+++ app/helpers/overtimes_helper.rb
@@ -1,2 +1,20 @@
 module OvertimesHelper
+  def extract_date(date)
+    date.strftime("%e")
+  end
+
+  def extract_day(date)
+    wd = ["日", "月", "火", "水", "木", "金", "土"]
+    date.strftime("#{wd[date.wday]}")
+  end
+
+  def extract_time(date_time)
+    date_time.strftime("%H:%M")
+  end
+
+  def convert_to_hour_and_minute(in_minutes)
+    hour = in_minutes / 60
+    minute = in_minutes % 60
+    "#{sprintf("%02d", hour)}:#{sprintf("%02d", minute)}"
+  end
 end
