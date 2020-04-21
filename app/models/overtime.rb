47fb612aca9841412dbe583421365381b67c440d
togashi29 <saladwithsalad@yahoo.co.jp>
2020-04-21 22:44:54 +0900

    group_monthly_chart_data method

--- app/models/overtime.rb
+++ app/models/overtime.rb
@@ -14,12 +14,35 @@ class Overtime < ApplicationRecord
   def self.monthly_chart_data(userid)
     # {[2019, 4]=>○○○○, [2019, 5]=>○○○○, …} を生成
     year_and_month_minute_data = Overtime.where(user_id: userid).group("extract(year from date)").group("extract(month from date)").sum(:work_time_minute)
+    # {["2019年4月"=>○○○○, "2019年4月"=>○○○○, …} に成形
     monthly_hour_data = {}
     year_and_month_minute_data.each do |key, value|
       monthly_hour_data["#{key[0].floor}年#{key[1].floor}月"] = (value.to_f / 60).floor(1)
     end
     monthly_hour_data
   end
+
+
+  # index-chart用
+  def self.group_monthly_hour_data
+    # {["A", "2019年4月"]=>1828, ["A", "2019年5月"]=>6418, ...} を生成
+    group_monthly_minute_data = Overtime.joins(:user).select("overtimes*, users*").group("users.group").group("DATE_FORMAT(date,'%Y年%c月')").sum(:work_time_minute)
+    hash = {}
+    group_monthly_minute_data.each do |key, value|
+      hash[key] = (value.to_f / 60).floor(1)
+    end
+    hash
+  end
+
+  def self.group_monthly_chart_data
+    # {"A"=> {"2019年4月"=>30.4, "2019年5月"=>106.9, ...} に成形
+    hash = Hash.new { |h,k| h[k] = {} }
+    Overtime.group_monthly_hour_data.each do |key, value|
+      hash[key[0]][key[1]] = value
+    end
+    hash
+  end
+
 
   private
 
