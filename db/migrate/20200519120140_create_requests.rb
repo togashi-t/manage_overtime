b1e571a4ff266968a338a811a0e59773fde83a48
togashi29 <saladwithsalad@yahoo.co.jp>
2020-05-19 21:08:24 +0900

    add null:false

--- db/migrate/20200519120140_create_requests.rb
+++ db/migrate/20200519120140_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-      t.string :detail
-      t.references :user, foreign_key: true
+      t.string :detail, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
