c34ad04980c6bbdfa4f9ea61a14074e8a3c94585
togashi29 <saladwithsalad@yahoo.co.jp>
2020-05-31 20:24:47 +0900

    user validation test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    name { Gimei.unique.name.kanji }
+    group { ("A".."Z").to_a.sample }
+    sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
+    password { Faker::Internet.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # バリデーション
+  context "name, group, email, passwordが入力されている" do
+    it "ユーザーが作られる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  context "nameが入力されていない" do
+    it "エラーする" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors.messages[:name]).to include "を入力してください"
+    end
+  end
+
+  context "groupが入力されていない" do
+    it "エラーする" do
+      user = build(:user, group: nil)
+      user.valid?
+      expect(user.errors.messages[:group]).to include "を入力してください"
+    end
+  end
+
+  context "emailが入力されていない" do
+    it "エラーする" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors.messages[:email]).to include "を入力してください"
+    end
+  end
+
+  context "同一のemailが存在する" do
+    it "エラーする" do
+      create(:user, email: "tsuruoka@example.com")
+      user = build(:user, email: "tsuruoka@example.com")
+      user.valid?
+      expect(user.errors.messages[:email]).to include "はすでに存在します"
+    end
+  end
+
+  context "passwordが入力されていない" do
+    it "エラーする" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors.messages[:password]).to include "を入力してください"
+    end
+  end
+
 end
