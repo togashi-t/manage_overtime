522dd180154ac1a223ae02d2f8b5407f1c734832
togashi29 <saladwithsalad@yahoo.co.jp>
2020-06-04 22:17:35 +0900

    guest user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users, only: [:index, :show]
   resource :overtimes, only: [:create, :update, :destroy]
   resources :requests, only: [:create, :edit, :update, :destroy]
