65f7c9e730f2e6cd87f8d6fa31e6700594b489f0
togashi29 <saladwithsalad@yahoo.co.jp>
2020-06-04 22:18:57 +0900

    rubocop -a

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
   def new_guest
-    user = User.find_or_create_by(email: "guest@example.com") do |user|
+    user = User.find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲスト"
       user.group = "Z"
     end
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
   end
 end
