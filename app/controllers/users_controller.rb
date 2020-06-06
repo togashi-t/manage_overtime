a9e875d7c7b3a8f048e9658d897129754ca5acce
togashi29 <saladwithsalad@yahoo.co.jp>
2020-06-06 11:44:32 +0900

    delete unnecessary code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,5 @@ class UsersController < ApplicationController
     gon.overtimes_devided_into_hour_and_minute = User.find(params[:id]).overtimes_devided_into_hour_and_minute
     gon.monthly_chart_data = User.find(params[:id]).monthly_chart_data
     @user = User.find(params[:id])
-    # @overtimes = User.find(params[:id]).this_month_overtimes
-    # @requests = User.find(params[:id]).requests
   end
 end
