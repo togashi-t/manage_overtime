101e8313e0b893fafbfd42119ed1c56f4d764faa
togashi29 <saladwithsalad@yahoo.co.jp>
2020-06-06 11:01:12 +0900

    use method in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def show
     gon.overtimes_devided_into_hour_and_minute = User.find(params[:id]).overtimes_devided_into_hour_and_minute
     gon.monthly_chart_data = User.find(params[:id]).monthly_chart_data
-    @overtimes = User.find(params[:id]).this_month_overtimes
-    @requests = User.find(params[:id]).requests
+    @user = User.find(params[:id])
+    # @overtimes = User.find(params[:id]).this_month_overtimes
+    # @requests = User.find(params[:id]).requests
   end
 end
