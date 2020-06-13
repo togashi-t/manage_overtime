bce98453424829a9c99c649c74f17e3edc7508f9
togashi29 <saladwithsalad@yahoo.co.jp>
2020-06-13 10:37:59 +0900

    set up capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server 'manage-overtime.work', user: "togashi", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -41,11 +41,7 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, keys: '~/.ssh/manage_overtime_key_rsa'
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
